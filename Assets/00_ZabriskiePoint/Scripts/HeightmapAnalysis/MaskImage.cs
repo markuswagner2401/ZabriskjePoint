a4d9e63d6dcd48846643e76fb0ddb8747112d21c
MW2401 <markuswagner2401@googlemail.com>
2023-08-10 15:26:32 +0200

    vor einbau neue videos

--- Assets/00_ZabriskiePoint/Scripts/HeightmapAnalysis/MaskImage.cs
+++ Assets/00_ZabriskiePoint/Scripts/HeightmapAnalysis/MaskImage.cs
@@ -76,5 +76,43 @@ public class MaskImage : MonoBehaviour
 
         return targetTexture;
     }
+
+    public RenderTexture GetMaskedImage(Texture inputTexture, RenderTexture targetTexture, int maskPercentage, float verticalShift, float horizontalShift, float rotation)
+    {
+        // Calculate the dimensions of the masked image
+        int maskWidth = inputTexture.width * maskPercentage / 100;
+        int maskHeight = inputTexture.height * maskPercentage / 100;
+
+        // Create the output texture
+        RenderTexture outputTexture = new RenderTexture(maskWidth, maskHeight, 0);
+        outputTexture.enableRandomWrite = true;
+        outputTexture.Create();
+
+        // Find the kernel in the shader
+        int maskKernel = computeShader.FindKernel("MaskImage");
+
+        // Set parameters for the shader
+        computeShader.SetTexture(maskKernel, "inputTexture", inputTexture);
+        computeShader.SetTexture(maskKernel, "outputTexture", outputTexture);
+        computeShader.SetInt("maskPercentage", maskPercentage);
+        computeShader.SetInts("inputResolution", new int[] { inputTexture.width, inputTexture.height });
+        computeShader.SetInts("outputResolution", new int[] { outputTexture.width, outputTexture.height });
+
+        computeShader.SetFloat("verticalShift", verticalShift);
+        computeShader.SetFloat("horizontalShift", horizontalShift);
+
+        float rotationAngleInRadians = Mathf.Deg2Rad * rotation;
+        computeShader.SetFloat("rotationAngle", rotationAngleInRadians);
+
+        // Dispatch the shader
+        computeShader.Dispatch(maskKernel, outputTexture.width / 8, outputTexture.height / 8, 1);
+
+        // Return the output texture
+        Graphics.Blit(outputTexture, targetTexture);
+
+        outputTexture.Release();
+
+        return targetTexture;
+    }
 }
 
